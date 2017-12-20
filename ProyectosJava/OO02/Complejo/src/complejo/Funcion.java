c43636b04468df58d4b5c1d1e614255af7e0585c
Albertopatat <albertopatat@gmail.com>
2017-12-20 00:08:16 -0300

    Primera version. Creando clases.

---
+++ ProyectosJava/OO02/Complejo/src/complejo/Funcion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package complejo;
+
+/**
+ *
+ * @author Alberto
+ */
+public class Funcion {
+    
+    private String diaSemana;
+    private String duracion;
+    private String horaInicio;
+    private int numero;
+    
+    public Funcion(){
+        }
+    
+    public void calcularDisponibilidad(){
+        }
+    
+    public void capacidadSala(){
+        
+    }
+    
+    public void estaEnCurso(){
+        
+    }
+    
+    public void hayLugar(){
+        
+    }
+    
+    public void mostrarDiaHora(){
+        
+    }
+    
+}   
+
