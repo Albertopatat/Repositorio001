a77c4465842625cb02519fd5ca184ff7060688c4
Albertopatat <albertopatat@gmail.com>
2017-12-21 22:13:59 -0300

    Primera version. Con todas las clases, según el apunte.

---
+++ ProyectosJava/Complejo/src/complejo/Entrada.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package complejo;
+
+/**
+ *
+ * @author Alberto
+ */
+public class Entrada {
+
+    private String fechaHoraFuncion;
+    private String fechaHoraVenta;
+    private float precioCobrado;
+    private int ticketNro;
+    
+public Entrada(){
+    
+}   
+
+public void estaAnulado(){
+    //return 
+}
+
+}
---
+++ ProyectosJava/Complejo/src/complejo/Programacion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package complejo;
+
+/**
+ *
+ * @author Alberto
+ */
+public class Programacion {
+
+    private String fechaFin;
+    private String fechaHoraCreada;
+    private String fechaInicio;
+    
+    public Programacion(){
+        
+    }
+    
+    public void estaCompleta(){
+        
+    }
+    
+    public void estaIniciadaFuncion(){
+        
+    }
+    
+    public void estaVigente(){
+        
+    }
+    
+    public void mostrarProgramacion(){
+        
+    }
+    
+}
